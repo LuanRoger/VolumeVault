759af8068dd141ab5f0d4328c570bef3677a5c24
LuanRoger <luan.roger.2003@gmail.com>
2023-02-15 09:57:40 -0300

    refactor(environment): rename environment variables

--- VolumeVaultInfra/VolumeVaultInfra/Utils/EnvVariableConsts.cs
+++ VolumeVaultInfra/VolumeVaultInfra/Utils/EnvVariableConsts.cs
@@ -5,9 +5,9 @@ public static class EnvVariableConsts
     public const string API_KEY = "API_KEY";
     public const string JWT_SYMMETRIC_KEY = "JWT_SYMMETRIC_KEY";
     public const string ASPNETCORE_ENVIRONMENT = "ASPNETCORE_ENVIRONMENT";    
-    public const string POSTGRES_HOST = "POSTGRES_HOST";
-    public const string POSTGRES_PORT = "POSTGRES_PORT";
-    public const string DEFAULT_POSTGRES_DB_NAME = "DEFAULT_POSTGRES_DB_NAME";
+    public const string POSTGRES_HOST = "VV_POSTGRES_HOST";
+    public const string POSTGRES_PORT = "VV_POSTGRES_PORT";
+    public const string DEFAULT_POSTGRES_DB_NAME = "VV_DEFAULT_POSTGRES_DB_NAME";
     public const string VV_POSTGRES_USER = "VV_POSTGRES_USER";
     public const string VV_POSTGRES_PASSWORD = "VV_POSTGRES_PASSWORD";
     public const string VV_MONGO_HOST = "VV_MONGO_HOST";
