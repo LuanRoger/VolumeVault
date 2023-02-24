fcc5418d36cfd752b4d2b20c79a6ceaa2a55dbc9
LuanRoger <luan.roger.2003@gmail.com>
2023-02-24 08:48:25 -0300

    feat(theme): add system theme brightness

--- lib/providers/interfaces/theme_preferences_state.dart
+++ lib/providers/interfaces/theme_preferences_state.dart
@@ -1,8 +1,9 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:volume_vault/models/enums/theme_brightness.dart';
 import 'package:volume_vault/shared/preferences/models/theme_preferences.dart';
 
 class ThemePreferencesState extends StateNotifier<ThemePreferences> {
   ThemePreferencesState({ThemePreferences? themePreferences})
-      : super(themePreferences ??
-            const ThemePreferences(darkModePreference: false));
+      : super(
+            themePreferences ?? const ThemePreferences(themeBrightnes: ThemeBrightness.LIGHT));
 }
