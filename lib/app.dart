ab4d4961ba472bbbbb6a04b9c5a35a700896fc90
LuanRoger <luan.roger.2003@gmail.com>
2023-03-01 08:21:11 -0300

    refactor(main): add more provider scope and separate app from main

---
+++ lib/app.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
+import 'package:volume_vault/providers/providers.dart';
+import 'package:volume_vault/shared/routes/app_routes.dart';
+import 'package:volume_vault/shared/routes/route_driver.dart';
+import 'package:volume_vault/shared/theme/app_theme.dart';
+
+class App extends ConsumerWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeBrightness =
+        ref.watch(themePreferencesStateProvider).themeBrightnes;
+    final userSession = ref.watch(userSessionNotifierProvider);
+
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: themeBrightness.themeMode,
+        theme: lightTheme(context),
+        darkTheme: darkTheme(context),
+        onGenerateRoute: RouteDriver.driver,
+        initialRoute: userSession.maybeWhen(
+          data: (userSession) => userSession.token.isEmpty ? AppRoutes.loginPageRoute : AppRoutes.homePageRoute,
+          orElse: () => AppRoutes.loginPageRoute),
+        builder: (context, child) => ResponsiveWrapper.builder(
+              child,
+              minWidth: 375,
+              defaultScale: true,
+              breakpoints: const [
+                ResponsiveBreakpoint.resize(375, name: MOBILE),
+                ResponsiveBreakpoint.resize(1200, name: DESKTOP)
+              ],
+            ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,61 +2,61 @@ import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:volume_vault/app.dart';
+import 'package:volume_vault/models/enums/theme_brightness.dart';
+import 'package:volume_vault/providers/interfaces/graphics_preferences_state.dart';
+import 'package:volume_vault/providers/interfaces/theme_preferences_state.dart';
 import 'package:volume_vault/providers/providers.dart';
 import 'package:volume_vault/shared/preferences/app_preferences.dart';
-import 'package:volume_vault/shared/routes/app_routes.dart';
-import 'package:volume_vault/shared/routes/route_driver.dart';
-import 'package:volume_vault/shared/theme/app_theme.dart';
-import 'package:responsive_framework/responsive_framework.dart';
+import 'package:volume_vault/shared/preferences/models/graphics_preferences.dart';
+import 'package:volume_vault/shared/preferences/models/theme_preferences.dart';
+import 'package:volume_vault/shared/preferences/preferences_key.dart';
 import 'package:volume_vault/shared/utils/platform_utils.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final appPreferences = AppPreferences(await SharedPreferences.getInstance());
-  await appPreferences.loadPreferences();
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  final appPreferences = loadPreferences(sharedPreferences);
 
   runApp(
     ProviderScope(overrides: [
-      appPreferencesProvider.overrideWith((_) => appPreferences),
+      themePreferencesStateProvider.overrideWith((_) {
+        final themePrefences = appPreferences.themePreferences;
+
+        return ThemePreferencesState(sharedPreferences,
+            themePreferences: themePrefences);
+      }),
+      graphicsPreferencesStateProvider.overrideWith((_) {
+        final graphicsPreferences = appPreferences.graphicsPreferences;
+
+        return GraphicsPreferencesState(sharedPreferences,
+            graphicsPreferences: graphicsPreferences);
+      }),
     ], child: const App()),
   );
 
-  if(PlatformUtils.isDesktop) {
+  if (PlatformUtils.isDesktop) {
     doWhenWindowReady(() {
-    const initialSize = Size(1200, 600);
-    appWindow.minSize = initialSize;
-    appWindow.size = initialSize;
-    appWindow.title = "Volume Vault";
-    appWindow.alignment = Alignment.center;
-    appWindow.show();
-  });
+      const initialSize = Size(1200, 600);
+      appWindow.minSize = initialSize;
+      appWindow.size = initialSize;
+      appWindow.title = "Volume Vault";
+      appWindow.alignment = Alignment.center;
+      appWindow.show();
+    });
   }
 }
 
-class App extends ConsumerWidget {
-  const App({super.key});
+AppPreferences loadPreferences(SharedPreferences preferences) {
+  final themePreferences = ThemePreferences(
+      themeBrightnes: ThemeBrightness
+          .values[preferences.getInt(PreferencesKey.themeModePrefKey) ?? 0]);
+  final graphicsPreferences = GraphicsPreferences(
+      lightEffect:
+          preferences.getBool(PreferencesKey.lightEffectPrefKey) ?? true);
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final themeBrightness =
-        ref.watch(themePreferencesStateProvider).themeBrightnes;
-
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        themeMode: themeBrightness.themeMode,
-        theme: lightTheme(context),
-        darkTheme: darkTheme(context),
-        onGenerateRoute: RouteDriver.driver,
-        initialRoute: AppRoutes.homePageRoute,
-        builder: (context, child) => ResponsiveWrapper.builder(
-              child,
-              minWidth: 375,
-              defaultScale: true,
-              breakpoints: const [
-                ResponsiveBreakpoint.resize(375, name: MOBILE),
-                ResponsiveBreakpoint.resize(1200, name: DESKTOP)
-              ],
-            ));
-  }
+  return AppPreferences(
+      themePreferences: themePreferences,
+      graphicsPreferences: graphicsPreferences);
 }
