1d6fd676fdf870fc6f7049dbbd7721f5098853b6
LuanRoger <luan.roger.2003@gmail.com>
2023-03-09 07:54:05 -0300

    feat(search): replace search bar with button and dialog

---
+++ lib/shared/widgets/book_search_result_tile.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:volume_vault/models/book_search_result.dart';
+
+class BookSearchResultTile extends StatelessWidget {
+  final BookSearchResult bookSearchResult;
+  final void Function()? onTap;
+
+  const BookSearchResultTile(this.bookSearchResult, {super.key, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.book_rounded),
+      title: Text(
+        bookSearchResult.title,
+        style: Theme.of(context).textTheme.bodyLarge,
+      ),
+      subtitle: Text(bookSearchResult.author),
+      autofocus: true,
+      trailing: const Text("Ver informações"),
+      onTap: onTap,
+    );
+  }
+}
