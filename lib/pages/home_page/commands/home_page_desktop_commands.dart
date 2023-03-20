1c6b083d90b2b9e65b08788f0daf8e194108cd11
LuanRoger <luan.roger.2003@gmail.com>
2023-03-20 13:03:52 -0300

    feat(command): create commands and strategy for `HomePage` pages layout

---
+++ lib/pages/home_page/commands/home_page_desktop_commands.dart
@@ -0,0 +1,6 @@
+import 'package:volume_vault/pages/home_page/commands/home_page_layout_strategy.dart';
+
+class HomePageDesktopCommands extends HomePageLayoutStrategy {
+  const HomePageDesktopCommands();
+  //There's no exclusive behavior for desktop.
+}
--- lib/pages/home_page/layouts/home_page_desktop.dart
+++ lib/pages/home_page/layouts/home_page_desktop.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:volume_vault/models/enums/visualization_type.dart';
-import 'package:volume_vault/pages/home_page/commands/home_page_mobile_command.dart';
+import 'package:volume_vault/pages/home_page/commands/home_page_desktop_commands.dart';
+import 'package:volume_vault/pages/home_page/sections/layouts/home_section_desktop.dart';
 import 'package:volume_vault/pages/home_page/sections/layouts/home_section_mobile.dart';
 
-class HomePageMobile extends HookConsumerWidget {
-  final HomePageMobileCommands _commands = HomePageMobileCommands();
+class HomePageDesktop extends HookConsumerWidget {
+  final HomePageDesktopCommands _commands = const HomePageDesktopCommands();
 
-  HomePageMobile({super.key});
+  const HomePageDesktop({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -20,7 +21,7 @@ class HomePageMobile extends HookConsumerWidget {
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : HomeSectionMobile(
+          : const HomeSectionDesktop(
               viewType: VisualizationType.LIST,
             ),
     );
