c244ed9b3b4b11b02757ce25d5d79aa59395de76
LuanRoger <luan.roger.2003@gmail.com>
2023-03-21 11:19:22 -0300

    feat(widget): resurect `SearchTextField` and create placeholder

--- lib/shared/widgets/search_text_field.dart
+++ lib/shared/widgets/search_text_field.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class SearchTextField extends StatelessWidget {
+class SearchTextField extends HookWidget {
   double? width;
   double? height;
+  String? label;
 
-  SearchTextField({super.key, this.width, this.height});
+  final TextEditingController controller;
+
+  SearchTextField(
+      {super.key,
+      required this.controller,
+      this.width,
+      this.height,
+      this.label});
 
   @override
   Widget build(BuildContext context) {
+    useListenable(controller);
+
     return TextField(
+      controller: controller,
       decoration: InputDecoration(
-          prefixIcon: const Icon(Icons.search_rounded),
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50),
-              borderSide: BorderSide.none),
-          filled: true,
-          isDense: true,
-          contentPadding: EdgeInsets.all(8.0)),
+        prefixIcon: const Icon(Icons.search_rounded),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50),
+            borderSide: BorderSide.none),
+        hintText: label,
+        filled: true,
+        isDense: true,
+        contentPadding: const EdgeInsets.all(8.0),
+        suffixIcon: controller.text.isNotEmpty
+            ? IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: controller.clear,
+              )
+            : null,
+      ),
     );
   }
 }
