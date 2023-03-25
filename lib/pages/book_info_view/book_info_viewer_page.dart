e5e394ee3f9f777822f6906c7a85472e4f97204c
LuanRoger <luan.roger.2003@gmail.com>
2023-03-25 15:13:57 -0300

    fix(book viewer page): refresh info when edit

--- lib/pages/book_info_view/book_info_viewer_page.dart
+++ lib/pages/book_info_view/book_info_viewer_page.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:volume_vault/models/book_model.dart';
 import 'package:volume_vault/pages/book_info_view/commands/book_info_viewer_command.dart';
 import 'package:volume_vault/providers/providers.dart';
@@ -22,18 +23,33 @@ class BookInfoViewerPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isMounted = useIsMounted();
     final currentBookInfoState = useState(_book);
+    final hasChanges = useState(false);
     final book = currentBookInfoState.value;
 
     final isLoadingState = useState(false);
 
     return Scaffold(
         appBar: AppBar(
+          leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context, hasChanges.value)),
           actions: [
             IconButton(
-                onPressed: () => Navigator.pushNamed(
-                    context, AppRoutes.registerEditBookPageRoute,
-                    arguments: [book]),
+                onPressed: () async {
+                  final bool? success = await Navigator.pushNamed<bool>(
+                      context, AppRoutes.registerEditBookPageRoute,
+                      arguments: [book]);
+                  if (success == null || !success || !isMounted()) return;
+
+                  final BookModel? newInfoBook =
+                      await _command.refreshBookInfo(context, ref, book.id);
+                  if (newInfoBook == null) return;
+
+                  currentBookInfoState.value = newInfoBook;
+                  hasChanges.value = true;
+                },
                 icon: const Icon(Icons.edit_rounded)),
             if (book.buyLink != null)
               IconButton(
@@ -46,22 +62,14 @@ class BookInfoViewerPage extends HookConsumerWidget {
                 switch (value) {
                   case 0:
                     bool delete = await _command.showDeleteBookDialog(context);
-                    if (!delete) break;
+                    if (!delete || !isMounted()) break;
                     isLoadingState.value = true;
 
-                    final controller =
-                        await ref.read(bookControllerProvider.future);
-                    final result = await controller.deleteBook(book.id);
-                    if (!result) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Erro ao deletar livro"),
-                        ),
-                      );
-                    } else {
-                      Navigator.pop(context, true);
-                    }
-                    break;
+                    final bool success =
+                        await _command.deleteBook(context, ref, book.id);
+                    if (!success) break;
+
+                    Navigator.pop(context, true);
                 }
                 isLoadingState.value = false;
               },
@@ -74,23 +82,13 @@ class BookInfoViewerPage extends HookConsumerWidget {
                 child: CircularProgressIndicator(),
               )
             : BookInfoViwerBodyPage(
-                _book,
+                book,
                 onRefresh: () async {
-                  final controller =
-                      await ref.read(bookControllerProvider.future);
-                  final BookModel? newInfos =
-                      await controller.getBookInfoById(book.id);
+                  final BookModel? newInfoBook =
+                      await _command.refreshBookInfo(context, ref, book.id);
+                  if (newInfoBook == null) return;
 
-                  if (newInfos == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Erro ao atualizar informações"),
-                      ),
-                    );
-                    return;
-                  }
-
-                  currentBookInfoState.value = newInfos;
+                  currentBookInfoState.value = newInfoBook;
                 },
               ));
   }
@@ -98,9 +96,9 @@ class BookInfoViewerPage extends HookConsumerWidget {
 
 class BookInfoViwerBodyPage extends HookConsumerWidget {
   final BookModel book;
-  Future<void> Function() onRefresh;
+  final Future<void> Function() onRefresh;
 
-  BookInfoViwerBodyPage(this.book, {super.key, required this.onRefresh});
+  const BookInfoViwerBodyPage(this.book, {super.key, required this.onRefresh});
 
   Future<Widget> _buildCoverShowcase(String coverLink,
       {required Size showcaseSize,
@@ -127,129 +125,126 @@ class BookInfoViwerBodyPage extends HookConsumerWidget {
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: RefreshIndicator(
-        onRefresh: onRefresh,
-        child: SingleChildScrollView(
-          physics: const AlwaysScrollableScrollPhysics(),
-          child: Column(children: [
-            if (book.coverLink != null)
-              SizedBox(
-                  height: 250 * 1.2,
-                  width: size.width,
-                  child: FutureBuilder(
-                    future: _buildCoverShowcase(book.coverLink!,
-                        showcaseSize: Size(size.width, 250 * 1.2),
-                        context: context,
-                        renderLightEffect: graphicsPreferences.lightEffect),
-                    builder: (_, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                      if (snapshot.hasError || !snapshot.hasData) {
-                        return const SizedBox();
-                      }
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: Column(children: [
+          if (book.coverLink != null)
+            SizedBox(
+                height: 250 * 1.2,
+                width: size.width,
+                child: FutureBuilder(
+                  future: _buildCoverShowcase(book.coverLink!,
+                      showcaseSize: Size(size.width, 250 * 1.2),
+                      context: context,
+                      renderLightEffect: graphicsPreferences.lightEffect),
+                  builder: (_, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError || !snapshot.hasData) {
+                      return const SizedBox();
+                    }
 
-                      return snapshot.data!;
-                    },
-                  )),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  book.title,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                Text(
-                  book.author,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-              ],
-            ),
-            const SizedBox(height: 5),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Chip(
-                  avatar: const Icon(Icons.numbers_rounded),
-                  label:
-                      Text(book.edition != null ? "Ed. ${book.edition}" : "-"),
-                ),
-                Chip(
-                  avatar: const Icon(Icons.insert_drive_file_rounded),
-                  label: Text("${book.pagesNumber ?? "-"}"),
-                ),
-                Chip(
-                  avatar: const Icon(Icons.label_rounded),
-                  label: Text(book.genre ?? "-"),
-                )
-              ],
-            ),
-            const SizedBox(height: 5),
-            if (book.synopsis != null)
-              TitleCard(
-                title: "Sinopse",
-                content: book.synopsis!,
-                expand: true,
+                    return snapshot.data!;
+                  },
+                )),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                book.title,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
-            const SizedBox(height: 5),
-            const IconText(icon: Icons.menu_book_rounded, text: "Informações"),
-            ListView(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              children: [
-                ListTile(title: const Text("ISBN"), trailing: Text(book.isbn)),
-                if (book.publisher != null)
-                  ListTile(
-                      title: const Text("Editora"),
-                      trailing: Text(book.publisher!)),
-                if (book.publicationYear != null)
-                  ListTile(
-                      title: const Text("Ano de lançamento"),
-                      trailing: Text(book.publicationYear.toString())),
-                if (book.format != null)
-                  ListTile(
-                      title: const Text("Formato"),
-                      trailing: Text(book.format!.name)),
-                if (book.readed != null)
-                  ListTile(
-                      title: const Text("Lido"),
-                      trailing:
-                          book.readed! ? const Text("Sim") : const Text("Não")),
-                ListTile(
-                  title: const Text("Criado em"),
-                  trailing: Text(
-                    dayMonthYearFormat.format(book.createdAt),
-                  ),
-                ),
-                ListTile(
-                  title: const Text("Ultima modificação"),
-                  trailing: Text(
-                    dayMonthYearFormat.format(book.lastModification),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 5),
-            if (book.observation != null)
-              TitleCard(
-                title: "Obsevação",
-                content: book.observation!,
-                expand: true,
+              Text(
+                book.author,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-            const SizedBox(height: 5),
-            if (book.tags != null)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const IconText(icon: Icons.tag_rounded, text: "Tags"),
-                  const SizedBox(height: 5),
-                  ChipList(book.tags!.toSet()),
-                ],
+            ],
+          ),
+          const SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Chip(
+                avatar: const Icon(Icons.numbers_rounded),
+                label:
+                    Text(book.edition != null ? "Ed. ${book.edition}" : "-"),
+              ),
+              Chip(
+                avatar: const Icon(Icons.insert_drive_file_rounded),
+                label: Text("${book.pagesNumber ?? "-"}"),
+              ),
+              Chip(
+                avatar: const Icon(Icons.label_rounded),
+                label: Text(book.genre ?? "-"),
               )
-          ]),
-        ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          if (book.synopsis != null)
+            TitleCard(
+              title: "Sinopse",
+              content: book.synopsis!,
+              expand: true,
+            ),
+          const SizedBox(height: 5),
+          const IconText(icon: Icons.menu_book_rounded, text: "Informações"),
+          ListView(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              ListTile(title: const Text("ISBN"), trailing: Text(book.isbn)),
+              if (book.publisher != null)
+                ListTile(
+                    title: const Text("Editora"),
+                    trailing: Text(book.publisher!)),
+              if (book.publicationYear != null)
+                ListTile(
+                    title: const Text("Ano de lançamento"),
+                    trailing: Text(book.publicationYear.toString())),
+              if (book.format != null)
+                ListTile(
+                    title: const Text("Formato"),
+                    trailing: Text(book.format!.name)),
+              if (book.readed != null)
+                ListTile(
+                    title: const Text("Lido"),
+                    trailing:
+                        book.readed! ? const Text("Sim") : const Text("Não")),
+              ListTile(
+                title: const Text("Criado em"),
+                trailing: Text(
+                  dayMonthYearFormat.format(book.createdAt),
+                ),
+              ),
+              ListTile(
+                title: const Text("Ultima modificação"),
+                trailing: Text(
+                  dayMonthYearFormat.format(book.lastModification),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          if (book.observation != null)
+            TitleCard(
+              title: "Obsevação",
+              content: book.observation!,
+              expand: true,
+            ),
+          const SizedBox(height: 5),
+          if (book.tags != null)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const IconText(icon: Icons.tag_rounded, text: "Tags"),
+                const SizedBox(height: 5),
+                ChipList(book.tags!.toSet()),
+              ],
+            )
+        ]),
       ),
     );
   }
--- lib/pages/book_info_view/commands/book_info_viewer_strategy.dart
+++ lib/pages/book_info_view/commands/book_info_viewer_strategy.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:volume_vault/models/book_model.dart';
+import 'package:volume_vault/providers/providers.dart';
 
 abstract class BookInfoViewerStrategy {
   const BookInfoViewerStrategy();
+
+  Future<BookModel?> refreshBookInfo(
+      BuildContext context, WidgetRef ref, int bookId) async {
+    final bookController = await ref.read(bookControllerProvider.future);
+    final BookModel? newBookInfo = await bookController.getBookInfoById(bookId);
+
+    if (newBookInfo == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Erro ao atualizar informações"),
+        ),
+      );
+    }
+
+    return newBookInfo;
+  }
 
   Future<bool> showDeleteBookDialog(BuildContext context) async {
     bool delete = false;
@@ -29,6 +48,22 @@ abstract class BookInfoViewerStrategy {
     );
 
     return delete;
+  }
+
+  Future<bool> deleteBook(
+      BuildContext context, WidgetRef ref, int bookId) async {
+    final controller = await ref.read(bookControllerProvider.future);
+    final result = await controller.deleteBook(bookId);
+
+    if (!result && context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Erro ao deletar livro"),
+        ),
+      );
+    }
+
+    return result;
   }
 
   void launchBuyPage(String buyPageLink) async {
