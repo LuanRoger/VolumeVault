c9b5cfe20890d1f8a4920ac97c75e16c388c8b5c
LuanRoger <luan.roger.2003@gmail.com>
2023-04-29 16:56:31 -0300

    feat(widgets): create widgets for choose sort option

--- lib/shared/widgets/icon/icon_text.dart
+++ lib/shared/widgets/icon/icon_text.dart
@@ -3,18 +3,25 @@ import 'package:flutter/material.dart';
 class IconText extends StatelessWidget {
   final IconData icon;
   final String text;
+  final double? iconSize;
+  final TextStyle? textStyle;
 
-  const IconText({super.key, required this.icon, required this.text});
+  const IconText(
+      {super.key,
+      required this.icon,
+      required this.text,
+      this.iconSize,
+      this.textStyle});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Icon(icon),
+        Icon(icon, size: iconSize),
         const SizedBox(width: 5),
         Text(
           text,
-          style: Theme.of(context).textTheme.bodyLarge,
+          style: textStyle ?? Theme.of(context).textTheme.bodyLarge,
         )
       ],
     );
