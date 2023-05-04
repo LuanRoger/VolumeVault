46a80b5d56c52d2b1fe1966c6cbec17b968893ac
LuanRoger <luan.roger.2003@gmail.com>
2023-05-04 08:37:55 -0300

    feat(widget): create stateful `BottomSheet`

--- lib/shared/widgets/bottom_sheet/bottom_sheet.dart
+++ lib/shared/widgets/bottom_sheet/bottom_sheet.dart
@@ -1,68 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:volume_vault/shared/widgets/bottom_sheet/bottom_sheet_base.dart';
 
-class BottomSheet {
+class BottomSheet extends BottomSheetBase {
   List<Widget> items;
-  double padding;
-  Widget Function(BuildContext)? action;
-  bool dragable;
-  void Function()? onClose;
 
-  BottomSheet(
-      {required this.items,
-      this.action,
-      this.padding = 8.0,
-      this.dragable = false,
-      this.onClose});
+  BottomSheet({
+    required this.items,
+    super.padding,
+    super.dragable,
+    super.isDismissible,
+    super.isScrollControlled,
+    super.action,
+    super.onClose,
+  });
 
-  Future show(BuildContext context) {
-    return showModalBottomSheet(
-        context: context,
-        isDismissible: false,
-        enableDrag: dragable,
-        isScrollControlled: true,
-        useSafeArea: true,
-        builder: (context) {
-          return Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
-            child: Padding(
-              padding: EdgeInsets.all(padding),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            onClose?.call();
-                            Navigator.pop(context);
-                          },
-                          icon: const Icon(Icons.close_rounded)),
-                      if (dragable) const _DragIndicator(),
-                      action?.call(context) ?? const SizedBox(),
-                    ],
-                  ),
-                  const SizedBox(height: 15),
-                  ...items
-                ],
-              ),
-            ),
-          );
-        });
-  }
-}
-
-class _DragIndicator extends StatelessWidget {
-  const _DragIndicator();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 4,
-      width: 32,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color:
-              Theme.of(context).colorScheme.onSurfaceVariant.withOpacity(0.4)),
-    );
+  Future<void> show(BuildContext context) async {
+    return await super.showBottomSheet(context,
+        builder: (context) => Column(children: items));
   }
 }
---
+++ lib/shared/widgets/bottom_sheet/drag_indicator.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DragIndicator extends StatelessWidget {
+  const DragIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 4,
+      width: 32,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color:
+              Theme.of(context).colorScheme.onSurfaceVariant.withOpacity(0.4)),
+    );
+  }
+}
