36fa66177cc60706721b6044730801712ba4cc5b
LuanRoger <luan.roger.2003@gmail.com>
2023-05-12 15:55:50 -0300

    feat(migrations): add `Initial` migration on Hug service

---
+++ VolumeVaultInfra/VolumeVaultInfra.Book.Hug/Migrations/20230512185157_Initial.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace VolumeVaultInfra.Book.Hug.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Genre = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.ID);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Tag = table.Column<string>(type: "varchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.ID);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "UserIdentifier",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserIdentifier = table.Column<string>(type: "varchar(255)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserIdentifier", x => x.ID);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "longtext", nullable: false),
+                    ISBN = table.Column<string>(type: "varchar(17)", maxLength: 17, nullable: false),
+                    PublicationYear = table.Column<int>(type: "int", nullable: true),
+                    Publisher = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Edition = table.Column<int>(type: "int", nullable: true),
+                    PagesNumber = table.Column<int>(type: "int", nullable: true),
+                    Format = table.Column<int>(type: "int", nullable: true),
+                    Obsevation = table.Column<string>(type: "longtext", nullable: true),
+                    Synopsis = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: true),
+                    CoverLink = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: true),
+                    BuyLink = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: true),
+                    Readed = table.Column<int>(type: "int", nullable: true),
+                    ReadStartDay = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    ReadEndDay = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LastModification = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ownerid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Books_UserIdentifier_ownerid",
+                        column: x => x.ownerid,
+                        principalTable: "UserIdentifier",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BookGenre",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Book = table.Column<int>(type: "int", nullable: false),
+                    Genre = table.Column<int>(type: "int", nullable: false),
+                    UserIdentifier = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookGenre", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_BookGenre_Books_Book",
+                        column: x => x.Book,
+                        principalTable: "Books",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookGenre_Genres_Genre",
+                        column: x => x.Genre,
+                        principalTable: "Genres",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookGenre_UserIdentifier_UserIdentifier",
+                        column: x => x.UserIdentifier,
+                        principalTable: "UserIdentifier",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "BookTag",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookTag", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_BookTag_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookTag_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookGenre_Book",
+                table: "BookGenre",
+                column: "Book");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookGenre_Genre",
+                table: "BookGenre",
+                column: "Genre");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookGenre_UserIdentifier",
+                table: "BookGenre",
+                column: "UserIdentifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_ownerid",
+                table: "Books",
+                column: "ownerid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTag_BookId",
+                table: "BookTag",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTag_TagId",
+                table: "BookTag",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Genres_Genre",
+                table: "Genres",
+                column: "Genre",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Tag",
+                table: "Tags",
+                column: "Tag",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserIdentifier_UserIdentifier",
+                table: "UserIdentifier",
+                column: "UserIdentifier",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookGenre");
+
+            migrationBuilder.DropTable(
+                name: "BookTag");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "UserIdentifier");
+        }
+    }
+}
