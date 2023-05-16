11a1dd7d261ad097b8b7e238025ae2048602720b
LuanRoger <luan.roger.2003@gmail.com>
2023-05-16 08:58:30 -0300

    feat(firebase): start firebase

--- lib/shared/storage/local_storage_configuration.dart
+++ lib/shared/storage/local_storage_configuration.dart
@@ -4,4 +4,4 @@ import 'package:volume_vault/shared/storage/models/user_session.dart';
 
 final realmConfiguration =
     Configuration.local([UserSession.schema, ServerConfig.schema]);
-late final Realm realm = Realm(realmConfiguration);
+final Realm realm = Realm(realmConfiguration);
