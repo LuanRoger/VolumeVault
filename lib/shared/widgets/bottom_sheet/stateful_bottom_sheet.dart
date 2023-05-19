574119a58c5147341842dffaf7247ceb3ff71b1a
LuanRoger <luan.roger.2003@gmail.com>
2023-05-19 14:15:04 -0300

    feat(widget): update bottom sheet's drag indicator to M3

--- lib/shared/widgets/bottom_sheet/stateful_bottom_sheet.dart
+++ lib/shared/widgets/bottom_sheet/stateful_bottom_sheet.dart
@@ -4,14 +4,17 @@ import 'package:volume_vault/shared/widgets/bottom_sheet/bottom_sheet_base.dart'
 class StatefulBottomSheet extends BottomSheetBase {
   Widget Function(BuildContext, void Function(void Function())) child;
 
-  StatefulBottomSheet(
-      {required this.child,
-      super.dragable,
-      super.isDismissible,
-      super.isScrollControlled,
-      super.padding,
-      super.action,
-      super.onClose});
+  StatefulBottomSheet({
+    required this.child,
+    super.dragable,
+    super.isDismissible,
+    super.isScrollControlled,
+    super.padding,
+    super.action,
+    super.onClose,
+    super.mainAxisAlignment,
+    super.crossAxisAlignment,
+  });
 
   Future<void> show(BuildContext context) async {
     return await super.showBottomSheet(
