df1f2fff7cc7bf01607faaf0bd37d998ca2d1c04
LuanRoger <luan.roger.2003@gmail.com>
2023-06-16 14:09:24 -0300

    feat(profile): implement profile card on `HomePageSection`

--- lib/pages/home_page/layouts/home_page_desktop.dart
+++ lib/pages/home_page/layouts/home_page_desktop.dart
@@ -1,14 +1,11 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:volume_vault/models/book_model.dart';
 import 'package:volume_vault/pages/home_page/commands/home_page_desktop_commands.dart';
 import 'package:volume_vault/pages/home_page/sections/home_section/layouts/home_section_desktop.dart';
-import 'package:volume_vault/pages/home_page/sections/profile_section/profile_section.dart';
 import 'package:volume_vault/pages/register_edit_book_page/register_edit_book_page.dart';
 import 'package:volume_vault/shared/hooks/paging_controller_hook.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePageDesktop extends HookConsumerWidget {
   // ignore: unused_field
@@ -21,58 +18,21 @@ class HomePageDesktop extends HookConsumerWidget {
     final fetcherListController =
         usePagingController<int, BookModel>(firstPageKey: 1);
 
-    final navigationIndexState = useState(0);
-    final pageController = usePageController(initialPage: 0);
-
     return Scaffold(
-      body: Row(
-        children: [
-          Flexible(
-            flex: 0,
-            child: NavigationRail(
-              leading: OpenContainer(
-                clipBehavior: Clip.none,
-                openColor: Theme.of(context).colorScheme.background,
-                closedColor: Theme.of(context).colorScheme.background,
-                onClosed: (_) => fetcherListController.refresh(),
-                closedShape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18)),
-                closedBuilder: (_, open) => FloatingActionButton(
-                    onPressed: open, child: const Icon(Icons.add_rounded)),
-                openBuilder: (_, __) => RegisterEditBookPage(),
-              ),
-              destinations: [
-                NavigationRailDestination(
-                  icon: const Icon(Icons.home_rounded),
-                  label: Text(
-                      AppLocalizations.of(context)!.homeSectionLabelHomePage),
-                ),
-                NavigationRailDestination(
-                  icon: const Icon(Icons.person_rounded),
-                  label:
-                      Text(AppLocalizations.of(context)!.profileSectionNavBar),
-                )
-              ],
-              onDestinationSelected: (newValue) {
-                navigationIndexState.value = newValue;
-                pageController.jumpToPage(newValue);
-              },
-              selectedIndex: navigationIndexState.value,
-              labelType: NavigationRailLabelType.all,
-            ),
-          ),
-          Expanded(
-            child: PageView(
-              controller: pageController,
-              physics: const NeverScrollableScrollPhysics(),
-              children: [
-                HomeSectionDesktop(fetcherListController),
-                ProfileSection()
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: HomeSectionDesktop(fetcherListController),
+        ),
+        floatingActionButton: OpenContainer(
+          clipBehavior: Clip.none,
+          openColor: Theme.of(context).colorScheme.background,
+          closedColor: Theme.of(context).colorScheme.background,
+          onClosed: (_) => fetcherListController.refresh(),
+          closedShape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+          closedBuilder: (_, open) => FloatingActionButton(
+              onPressed: open, child: const Icon(Icons.add_rounded)),
+          openBuilder: (_, __) => const RegisterEditBookPage(),
+        ));
   }
 }
