cb94d9ce2548f61ecfdcc277ebcf8c67167d5f23
LuanRoger <luan.roger.2003@gmail.com>
2023-06-29 07:43:36 -0300

    feat(email identifier): create email identifier

---
+++ VolumeVaultInfra/VolumeVaultInfra.Book.Hug/Repositories/EmailUserIdentifierRepository.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using VolumeVaultInfra.Book.Hug.Contexts;
+using VolumeVaultInfra.Book.Hug.Models.Base;
+
+namespace VolumeVaultInfra.Book.Hug.Repositories;
+
+public class EmailUserIdentifierRepository : IEmailUserIdentifierRepository
+{
+    private DatabaseContext context { get; }
+
+    public EmailUserIdentifierRepository(DatabaseContext context)
+    {
+        this.context = context;
+    }
+    
+    public async Task<EmailUserIdentifier> EnsureEmailExists(EmailUserIdentifier? emailUserIdentifier)
+    {
+        EmailUserIdentifier? identifier = await context.emailUserIdentifiers.FirstOrDefaultAsync(emailIdentifier => emailIdentifier.email == emailUserIdentifier.email);
+        identifier ??= (await context.emailUserIdentifiers.AddAsync(emailUserIdentifier)).Entity;
+        
+        return identifier;
+    }
+
+    public async Task RelateToUser(string email, UserIdentifier userIdentifier)
+    {
+        EmailUserIdentifier? toRelate = await context.emailUserIdentifiers.FirstOrDefaultAsync(emailUser => emailUser.email == email);
+        if(toRelate is null) return;
+        
+        toRelate.userIdentifier = userIdentifier;
+    }
+
+
+    public async Task Flush() => await context.SaveChangesAsync();
+}
