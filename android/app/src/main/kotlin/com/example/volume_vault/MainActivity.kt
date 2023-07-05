c93948d9de9e79abf67fbc20efac56339846ad58
LuanRoger <luan.roger.2003@gmail.com>
2023-07-05 14:03:37 -0300

    feat(environment): create `dev`, `staging` and `prod` environment
    - Implement splash screen
    Powered by VeryGoodVentures

--- android/app/src/main/kotlin/com/example/volume_vault/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.volume_vault
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/luanroger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.luanroger.volume_vault
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
