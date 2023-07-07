7192c89d4b481363154a4744862cf58c42e4bed7
LuanRoger <luan.roger.2003@gmail.com>
2023-07-07 16:55:07 -0300

    chore(bootstrap): create new architecture's bootstrap

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
+import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:volume_vault/app.dart';
 import 'package:volume_vault/firebase_options.dart';
 import 'package:volume_vault/l10n/l10n.dart';
 import 'package:volume_vault/models/enums/theme_brightness.dart';
@@ -16,10 +17,32 @@ import 'package:volume_vault/shared/preferences/models/localization_preferences.
 import 'package:volume_vault/shared/preferences/models/theme_preferences.dart';
 import 'package:volume_vault/shared/preferences/preferences_keys.dart';
 
-void main() async {
+AppPreferences loadPreferences(SharedPreferences preferences) {
+  final themePreferences = ThemePreferences(
+      themeBrightnes: ThemeBrightness
+          .values[preferences.getInt(PreferencesKeys.themeModePrefKey) ?? 0]);
+  final graphicsPreferences = GraphicsPreferences(
+      lightEffect:
+          preferences.getBool(PreferencesKeys.lightEffectPrefKey) ?? true);
+  final localizationPreferences = LocalizationPreferences(
+      localization: L10n.getLocaleFromCode(
+          preferences.getInt(PreferencesKeys.localizationPrefKey) ?? 0));
+
+  return AppPreferences(
+      themePreferences: themePreferences,
+      graphicsPreferences: graphicsPreferences,
+      localizationPreferences: localizationPreferences);
+}
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
   WidgetsFlutterBinding.ensureInitialized();
 
-  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  // Add cross-flavor configuration here
+
+  final sharedPreferences = await SharedPreferences.getInstance();
   final appPreferences = loadPreferences(sharedPreferences);
 
   await Firebase.initializeApp(
@@ -46,23 +69,6 @@ void main() async {
         return LocalizationPreferencesState(sharedPreferences,
             localizationPreferences: localizationPreferences);
       }),
-    ], child: const App()),
+    ], child: await builder()),
   );
-}
-
-AppPreferences loadPreferences(SharedPreferences preferences) {
-  final themePreferences = ThemePreferences(
-      themeBrightnes: ThemeBrightness
-          .values[preferences.getInt(PreferencesKeys.themeModePrefKey) ?? 0]);
-  final graphicsPreferences = GraphicsPreferences(
-      lightEffect:
-          preferences.getBool(PreferencesKeys.lightEffectPrefKey) ?? true);
-  final localizationPreferences = LocalizationPreferences(
-      localization: L10n.getLocaleFromCode(
-          preferences.getInt(PreferencesKeys.localizationPrefKey) ?? 0));
-
-  return AppPreferences(
-      themePreferences: themePreferences,
-      graphicsPreferences: graphicsPreferences,
-      localizationPreferences: localizationPreferences);
 }
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:volume_vault/app.dart';
+import 'package:volume_vault/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
